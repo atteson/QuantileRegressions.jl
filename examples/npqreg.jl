843f25d0b26cc5673640699a993ef47959b22328
BuildTools <unconfigured@null.spigotmc.org>
2024-01-09 16:46:14 -0500

    minor update

--- examples/npqreg.jl
+++ examples/npqreg.jl
@@ -1,9 +1,12 @@
-using QuantileRegressions, Distributions, CSV
-mycyle = CSV.read("mcycle.csv")
+using QuantileRegressions, Distributions, CSV, DataFrames, Pkg, Plots
+dir = Pkg.dir( "QuantileRegressions" )
+mycyle = CSV.read(joinpath( dir, "examples", "mcycle.csv" ), DataFrame)
 x = mycyle.times
 y = mycyle.accel
 
+p = plot(size=[1200,1000])
 for t = range(0.01,0.99,length=99)
-res = QuantileRegressions.npqreg(Array(y), Array(x), t; h=2)
-plot!(res[1], res[2]; label="tau=$t")
+    res = QuantileRegressions.npqreg(Array(y), Array(x), t; h=2)
+    plot!(p, res[1], res[2]; label="tau=$t")
 end
+display(p)
